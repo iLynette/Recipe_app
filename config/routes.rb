ca82e6d4c4166490f0de5a945a924a92bd61402c
iLynette <lynetteacholah@icloud.com>
2022-06-27 18:09:47 +0300

    Set up all user files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+  end
+  root "users#index"
 end
