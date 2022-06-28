d08264c78ca888dcfba6a2711e916c4ef114181c
iLynette <lynetteacholah@icloud.com>
2022-06-28 11:38:46 +0300

    Add references to createfoodrecipes table

--- db/migrate/20220627154255_create_food_recipes.rb
+++ db/migrate/20220627154255_create_food_recipes.rb
@@ -2,6 +2,8 @@ class CreateFoodRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :food_recipes do |t|
       t.integer :quantity
+      t.references :recipe, index: true, null: false foreign_key: {to_table: :recipe}
+      t.references :food, index: true, null: false foreign_key: {to_table: :food}
 
       t.timestamps
     end
