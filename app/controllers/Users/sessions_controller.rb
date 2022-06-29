72a22abdd9a9e66049f106e34f11d70c32db02ba
iLynette <lynetteacholah@icloud.com>
2022-06-29 13:21:26 +0300

    Fix linter errors

--- app/controllers/Users/sessions_controller.rb
+++ app/controllers/Users/sessions_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
@@ -14,7 +12,7 @@ class Users::SessionsController < Devise::SessionsController
       # BackgroundWorker.trigger(resource)
     end
   end
-  
+
   # DELETE /resource/sign_out
   # def destroy
   #   super
