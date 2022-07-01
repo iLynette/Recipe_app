dd6c63da3672326b9f4f4d38c49e6c5929cafef5
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-01 11:48:15 -0700

    Update index and controller for shopping list

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,4 +1,7 @@
 class GeneralShoppingListsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @foods = Food.joins(food_recipes: :recipe).where(['recipes.user_id != ?', current_user.id]).map do
   end
 end
