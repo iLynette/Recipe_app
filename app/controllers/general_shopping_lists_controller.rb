77ed44b4d6d7000157b9710169be3d801babf768
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-01 13:10:23 -0700

    Refactor shopping list controller

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -2,6 +2,21 @@ class GeneralShoppingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @foods = Food.joins(food_recipes: :recipe).where(['recipes.user_id != ?', current_user.id]).map do
+      @total_price = 0
+      @foods = current_user.foods.includes(:food_recipes).map do |food|
+        next if food.food_recipes.empty?
+
+        total_quantity = food.food_recipes.sum(:quantity)
+        price = total_quantity * food.price
+        @total_price += price
+
+      {
+        name: food.name,
+        total_quantity:,
+        measurement_unit: food.measurement_unit,
+        price:
+      }
+    end.compact
+
   end
 end
