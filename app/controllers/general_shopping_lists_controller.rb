ac07dcfd2e3924c635e094ddef8a3dfc5d65a23a
usorfaitheloho <usorfaitheloho@gmail.com>
2022-07-01 16:37:32 -0700

    Fix linters

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -2,21 +2,18 @@ class GeneralShoppingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-      @total_price = 0
-      @foods = current_user.foods.includes(:food_recipes).map do |food|
-        next if food.food_recipes.empty?
+    @total_price = 0
+    @foods = current_user.foods.includes(:food_recipes).map do |food|
+      next if food.food_recipes.empty?
 
-        total_quantity = food.food_recipes.sum(:quantity)
-        price = total_quantity * food.price
-        @total_price += price
+      total_quantity = food.food_recipes.sum(:quantity)
+      price = total_quantity * food.price
+      @total_price += price
 
       {
         name: food.name,
-        total_quantity:,
-        measurement_unit: food.measurement_unit,
-        price:
+        measurement_unit: food.measurement_unit
       }
     end.compact
-
   end
 end
