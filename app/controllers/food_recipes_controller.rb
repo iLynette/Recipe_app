22b69d1a5f29cca7e29743eaf517c225c4b19351
iLynette <lynetteacholah@icloud.com>
2022-07-02 01:11:04 +0300

    Edit new, create & destroy methods in food recipes

--- app/controllers/food_recipes_controller.rb
+++ app/controllers/food_recipes_controller.rb
@@ -1,20 +1,19 @@
 class FoodRecipesController < ApplicationController
-  before_action :set_food_recipe, only: %i[show destroy]
 
   def new
-    @food_recipe = FoodRecipe.new
-    @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.all
+    
   end
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    food_recipe = FoodRecipe.new(params.require(:food_recipe).permit(:food_id, :quantity))
-    food_recipe.recipe = recipe
+    food_recipe = FoodRecipe.new(params.require(:recipe_food).permit(:food_id, :quantity))
+    food_recipe.recipe = @recipe
     respond_to do |format|
       format.html do
         if food_recipe.save
           flash[:success] = 'Ingredient saved successfully'
-          redirect_to recipe_url(recipe.id)
+          redirect_to recipe_url(@recipe.id)
         else
           flash[:error] = 'Error: Ingredient could not be saved'
           redirect_to new_recipe_food_recipe_url
@@ -25,19 +24,15 @@ class FoodRecipesController < ApplicationController
 
   def destroy
     @food_recipe = FoodRecipe.find(params[:id])
-    @recipe = food_recipe.recipe
+    @recipe = @food_recipe.recipe
     @food_recipe.destroy
     respond_to do |format|
-      format.html { redirect_to food_recipes_url, notice: 'Food recipe was successfully destroyed.' }
+      format.html { redirect_to recipe_url(@recipe), notice: 'Food recipe was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-
-  def set_food_recipe
-    @food_recipe = FoodRecipe.find(params[:id])
-  end
 
   def food_recipe_params
     params.require(:food_recipe).permit(:quantity)
